05d7e195d06c905a4276ee27180e40fe7f0b5610
shirasal <shira.salin@gmail.com>
2023-11-07 13:14:46 +0100

    added shiny app and data

---
+++ functions.R
@@ -0,0 +1,31 @@
+
+# GGPLOT THEME ------------------------------------------------------------
+
+theme_base <- function(){ 
+  font <- "Ubuntu"   # assign font family up front
+  
+  theme_minimal() %+replace%    # replace some elements
+    
+    theme(
+      
+      # panel and grid elements
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major.y = element_line(colour = "#c4cfcf"),
+      axis.ticks = element_blank(),
+      panel.border = element_blank(),
+      
+      # text elements
+      axis.title = element_text(
+        family = font,
+        size = 12,
+        face = "bold"),
+      
+      axis.text = element_text(
+        family = font,
+        size = 11),
+      
+      axis.text.x = element_text(
+        angle = 45, hjust = 1, vjust = 1.2)
+    )
+}
