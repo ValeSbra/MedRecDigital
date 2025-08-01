1065a1af81ae9bdeb8b63d67b224d6fa9d2913a5
beaslvdr <beasalva12@gmail.com>
2025-08-01 17:09:51 +0200

    Cambios en la Shiny

---
+++ Conversión_datos.R
@@ -0,0 +1,19 @@
+### Convertir los datos en porcentaje y reestructurar
+
+## Librerías y leer datos
+library(dplyr)
+library(tidyr)
+library(readr)
+
+datos = read.csv("C:/Users/Bea/Documents/ICM/MedDigital/Shiny App/MedRecDigital/data_for_analysis.csv")
+datos <- datos %>%
+  mutate(gsv_results = as.numeric(gsv_results))
+
+## Modificar los datos y reordenarlos
+# 1. Agrupar por país, id del país, año y especie, y sumar los resultados
+resumen <- datos %>%
+  group_by(species, country, country_code, year) %>%
+  summarise(gsv_results = sum(gsv_results, na.rm = TRUE), .groups = "drop")
+
+## Guardar el resultado como archivo .rds
+saveRDS(resumen, "GSV_data_sa.rds")
